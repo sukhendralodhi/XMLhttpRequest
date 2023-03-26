f2028ab10946cad32560df53cef900594213978f
sukhendra <mpct5jan1998@gmail.com>
2023-03-27 01:26:34 +0530

    Final touch

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 const btn = document.getElementById('btn');
+
+const getData = () => {
+    const http = new XMLHttpRequest();
+    http.open("GET", "users.json", true);
+
+    http.onload = function() {
+        if(this.status === 200) {
+            const usersData = JSON.parse(this.responseText);
+            console.log(usersData); 
+            let output = "";
+            usersData.forEach(user => {
+                output += `
+                <hr>
+                <ul>
+                    <li>Id: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>Age: ${user.age}</li>
+                    <li>Email: ${user.email}</li>
+                </ul>
+                `
+            });
+            document.getElementById('container').innerHTML = output;
+        }
+    }
+    http.send();
+}
+
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    getData();
+});
